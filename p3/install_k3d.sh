4dc279a1c96519fd02d71986232d36361adaa731
lyyrst <kurt.butorp@gmail.com>
2025-11-20 07:41:54 +0000

    p2 OK, start p3

---
+++ p3/install_k3d.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "=== Installation des dépendances ==="
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+echo "=== Ajout de la clé GPG de Docker ==="
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "=== Ajout du dépôt Docker ==="
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "=== Installation de Docker ==="
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "=== Ajout de l'utilisateur actuel au groupe docker ==="
+sudo usermod -aG docker "$USER"
+
+echo "=== Installation de k3d ==="
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+echo "=== Vérifications ==="
+docker --version
+k3d --version
+
+echo "=== Installation terminée ==="
+echo "⚠️ Déconnecte-toi / reconnecte-toi ou reboot pour appliquer le groupe docker."
